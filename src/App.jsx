2d37f2bea19d2a71a2d48b5cce9a5391a7d8d780
Danica Dombek <danica.dombek@gmx.de>
2021-05-11 16:22:25 +0200

    Add array in App.jsx to create Cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,33 @@ import * as React from 'react'
 import Pagination from './Pagination'
 import Cards from './Cards'
 
+const cards = [
+  {
+    title: 'Wie sieht es aus?',
+    text: 'Das ist die beste Antwort auf diese Frage.',
+    isBookmarked: false,
+    id: 1,
+  },
+  {
+    title: 'Frage',
+    text: 'Hier steht ein überaus sinnvoller und hilfreicher Text.',
+    isBookmarked: true,
+    id: 2,
+  },
+]
+
 export default () => {
   return (
     <div>
+      {cards.map(({ id, title, text, isBookmarked }) => (
+        <Cards
+          key={id}
+          title={title}
+          copyText={text}
+          isBookmarked={isBookmarked}
+        />
+      ))}
 
-      <Cards
-        title="Wie sieht es aus?"
-        copyText="Das ist die beste Antwort auf diese Frage."
-      />
-      <Cards
-        isBookmarked
-        title="Frage"
-        copyText="Hier steht ein überaus sinnvoller und hilfreicher Text."
-      />
       <Pagination isLeftDisabled text="1/34" />
     </div>
   )
